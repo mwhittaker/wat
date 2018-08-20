430fa12f5e6e8bc5ac6360e8d80a6f19798fa455
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-08-20 11:43:23 -0700

    Added limitations + revised wat prov spec section.
    One of the big pieces of feedback that we got from the reviewers is that
    we didn't explain the limitations of wat provenance enough. This commit
    adds/revises limitation subsections in the wat provenance and wat
    provenance specification sections.
    
    This commit also comments out some of the writing that the reviewers
    didn't like (e.g., automatic wat provenance specification checking).

--- paper/figures/redis_prov_spec.py
+++ paper/figures/redis_prov_spec.py
@@ -1,5 +1,6 @@
  def get_prov(T: List[Request], i: GetRequest):
    for a in reversed(T):
-     if isinstance(a, SetRequest) and a.key == i.key:
+     if (isinstance(a, SetRequest)
+         and a.key == i.key):
        return {[a]}
    return {[]}
