6157f7b80301302f50559be59ac6f561d0e530f6
Michael Whittaker <mjwhittaker@berkeley.edu>
2018-08-20 11:48:59 -0700

    Added comments to wat/state_machine.py

--- wat/state_machine.py
+++ wat/state_machine.py
@@ -4,13 +4,24 @@ Input = TypeVar('Input')
 Output = TypeVar('Output')
 
 class StateMachine(Generic[Input, Output]):
+    """A deterministic state machine.
+
+    A StateMachine represents a determinstic state machine. A StateMachine
+    begins in an initial start state. Upon receiving an input of type Input,
+    the state machine transitions into a new state and outputs an output of
+    type Output.
+    """
+
     def reset(self) -> None:
+        """Resets the state machine to its start state."""
         raise NotImplementedError()
 
     def transition(self, i: Input) -> Output:
+        """Transitions to a new state and outputs and ouput."""
         raise NotImplementedError()
 
     def run(self, inputs: List[Input]) -> List[Tuple[Input, Output]]:
+        """Performs a sequence of transitions from the start state."""
         self.reset()
         trace: List[Tuple[Input, Output]] = []
         for i in inputs:
